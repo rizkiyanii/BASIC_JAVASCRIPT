f36e467276ee43c0ad0d29972b567ba2b0e77c72
Rizkiyani <rzkyn0275@gmail.com>
2022-09-27 13:36:25 -1200

    pwpb_270922_basic JS Chapter 1

---
+++ commentVariable_1.js
@@ -0,0 +1,36 @@
+//single line comment
+/* 
+multiple line comment
+first chapter comment         
+*/
+var x = 23.5;
+var y = 23.5;
+var addiction = x+y;
+console.log(addiction)
+
+/* 
+multiple line comment
+second chapter is substraction        
+*/
+var x = 23;
+var y = 15;
+var substraction = x-y;
+console.log(substraction)
+
+/* 
+multiple line comment
+third chapter is division        
+*/
+var x = 40;
+var y = 5;
+var division = x/y;
+console.log(division)
+
+/* 
+multiple line comment
+foutrh chapter is multiplication       
+*/
+var x = 6;
+var y = 4;
+var multiplication = x*y;
+console.log(multiplication)
---
+++ mathOperator_4_latihan.js
@@ -0,0 +1,39 @@
+//increment
+console.log('====increment====')
+var a = 2
+var b = 12
+var a = b-a
+console.log(a)
+console.log(b)
+
+var c = 10
+var d = 2
+var c = c+d
+console.log(c)
+
+var e = 9
+var f = 3
+var e = e+f
+console.log(e)
+
+//decrement
+console.log('====decrement====')
+var g = 8
+var h = 2
+var g = g+h
+console.log(g)
+
+var i = 8
+var j = 0
+var i = i+j
+console.log(i)
+
+var k = 8
+var l = 0
+var k = k+i
+console.log(i)
+
+var m = 8
+var n = 0
+var m = m+n
+console.log(m)
